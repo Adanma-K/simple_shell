e1191a4c1007d65a5427a93d1feecc495f4d94f9
Ayomide <ilesanmibry@gmail.com>
2023-04-26 14:55:48 -0700

    feat: added new feature

---
+++ prints_env.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+
+/**
+  * print_env - env built-in, that prints the current environment
+  *
+  * Return: Nothing
+  */
+ 
+void _print_env(void)
+{
+	int i = 0, j = 0;
+
+	while (environ[i])
+	{
+		j = 0;
+		while (environ[i][j])
+		{
+			_putchar(environ[i][j]);
+			j++;
+		}
+
+		if (j != 0)
+			_putchar('\n');
+		i++;
+	}
+}
